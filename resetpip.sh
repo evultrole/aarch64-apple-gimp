361588931afe9313109ddf2b190fd7b064ee12b1
Steven Hillis <stevenhillis@All-in-One.local>
2021-03-19 18:11:18 -0600

    fixing compat across intel and arm

--- resetpip.sh
+++ resetpip.sh
@@ -1,5 +1,4 @@
-rm -rf .config/pip
-rm -rf .new_local
-rm -rf /var/folders/9d/8k9g4lrj3gvfd4fzxc2rx89c0000gn/T/pip*                           
-rm -rf Library/Caches/pip*
+rm -rf ~/.config/pip
+rm -rf ~/.new_local
+rm -rf ~/Library/Caches/pip*
 
